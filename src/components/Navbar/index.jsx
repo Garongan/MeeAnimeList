fd15168a7387406a4834cda8a05475391ef605fd
Garongan <alvindo56@gmail.com>
2023-12-04 11:27:21 +0700

    rebranding

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -30,14 +30,20 @@ const Navbar = () => {
   };
 
   return (
-    <header className={`${isScrolled ? 'top-5 z-10 container' : 'top-0 mx-0 px-0'} sticky transition-all duration-300`}>
-      <div className={`${isScrolled} ? bg-color-accent/75 backdrop-blur-sm rounded-lg : bg-color-accent shadow-md shadow-color-dark/30 transition-all duration-300`}>
-      <div className="container py-4 flex gap-2 justify-between md:items-center md:flex-row flex-col">
-        <Link href="/" className="uppercase font-bold text-[150%]">
-          mexanime
-        </Link>
-        <InputSearch />
-      </div>
+    <header
+      className={`${
+        isScrolled ? "top-5 z-10 container" : "top-0 mx-0 px-0"
+      } sticky transition-all duration-300`}
+    >
+      <div
+        className={`${isScrolled} ? bg-color-accent/75 backdrop-blur-sm rounded-lg : bg-color-accent shadow-md shadow-color-dark/30 transition-all duration-300`}
+      >
+        <div className="container py-4 flex gap-2 justify-between md:items-center md:flex-row flex-col">
+          <Link href="/" className="font-bold text-[150%]">
+            MeeAnimeList
+          </Link>
+          <InputSearch />
+        </div>
       </div>
     </header>
   );
